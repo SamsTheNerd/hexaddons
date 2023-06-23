f52cef56151d720b3914beda30493684946556ca
SamsTheNerd <sam@samsthenerd.com>
2023-06-23 16:41:25 -0400

    added all the addons data

--- cardgen.js
+++ cardgen.js
@@ -3,8 +3,8 @@ var getAddons = () => {
     var request = new XMLHttpRequest();
     request.open("GET", "./hexaddons.json", false);
     request.send(null)
-    var my_JSON_object = JSON.parse(request.responseText);
-    return my_JSON_object;
+    var allAddons = JSON.parse(request.responseText);
+    return allAddons;
 }
 
 
@@ -47,4 +47,47 @@ var genCard = (addon) => {
 var putCard = (addon, containerId) => {
     var container = document.getElementById(containerId);
     container.innerHTML += genCard(addon);
+}
+
+var genInteropCard = (addon) => {
+    var platformIcons = ``;
+    addon.platforms.forEach((platform) => {
+        platformIcons += `<img src="./platformIcons/${platform}Icon.png" alt="${platform} icon" class="platformIcon">`
+    })
+    if(addon.hex_provided){
+        platformIcons += "<span class='hexProvidedSpan'>|<img src='./otherIcons/hexxy.png' title='Hex Provided' alt='Hex Provided' class='platformIcon'></span>";
+    }
+
+    var links = ``;
+    if(addon.modrinth_url != null) {
+        links += `<a target="_blank" href="${addon.modrinth_url}" class="addonLink modrinthLink">Modrinth</a><br>`
+    }
+    if(addon.curseforge_url != null) {
+        links += `<a target="_blank" href="${addon.curseforge_url}" class="addonLink curseforgeLink">CurseForge</a><br>`
+    }
+    if(addon.book_url != null){
+        links += `<a target="_blank" href="${addon.book_url}" class="addonLink bookLink">Book</a><br>`
+    }
+    if(addon.source_url != null){
+        links += `<a target="_blank" href="${addon.source_url}" class="addonLink sourceLink">Source</a><br>`
+    }
+    
+
+    var card = `
+    <div class="addonCard interopCard" id="${addon.name}Card">
+        <div class="addonCardHeader">
+            <h3 class="addonTitle">${addon.name}
+            </h3>
+        </div>
+        <div class="platformShelf">${platformIcons}</div>
+        <p class="addonDescription">${addon.description}</p>
+        <p class="linkShelf">${links}</p>
+    </div>
+    `
+    return card;
+}
+
+var putInteropCard = (addon, containerId) => {
+    var container = document.getElementById(containerId);
+    container.innerHTML += genInteropCard(addon);
 }
--- main.js
+++ main.js
@@ -2,5 +2,12 @@
 
 window.onload = () => {
     var addons = getAddons();
-    addons.forEach((addon) => putCard(addon, "mainAddonGrid"));
+    addons.forEach((addon) => {
+        if(addon.type == "addon"){
+            putCard(addon, "mainAddonGrid")
+        }
+        if(addon.type == "interop"){
+            putInteropCard(addon, "interopGrid")
+        }
+    });
 }
