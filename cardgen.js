553c1a6203440558eb04cdccbced7bcdcd940f64
SamsTheNerd <sam@samsthenerd.com>
2023-06-23 20:01:41 -0400

    switched to link icons. added decent-ish mobile support. generally more responsive

--- cardgen.js
+++ cardgen.js
@@ -16,6 +16,31 @@ var getTools = () => {
 }
 
 
+var makeLinks = (addon) => {
+    var links = ``;
+    if(addon.modrinth_url != null) {
+        links += `<a target="_blank" href="${addon.modrinth_url}" class="addonLink modrinthLink">
+            <img src="./otherIcons/ModrinthIcon.png" title="Modrinth" alt="Modrinth Icon" class="linkIcon">
+        </a>`
+    }
+    if(addon.curseforge_url != null) {
+        links += `<a target="_blank" href="${addon.curseforge_url}" class="addonLink curseforgeLink">
+            <img src="./otherIcons/CurseforgeIcon.png" title="CurseForge" alt="CurseForge Icon" class="linkIcon">
+        </a>`
+    }
+    if(addon.book_url != null){
+        links += `<a target="_blank" href="${addon.book_url}" class="addonLink bookLink">
+            <img src="./otherIcons/BookIcon.png" title="Book" alt="Hexcasting Guide Book Icon" class="linkIcon bookIcon">
+        </a>`
+    }
+    if(addon.source_url != null){
+        links += `<a target="_blank" href="${addon.source_url}" class="addonLink sourceLink">
+            <img src="./otherIcons/GithubIcon.png" title="Source" alt="GitHub Icon" class="linkIcon">
+        </a>`
+    }
+    return links;
+}
+
 // given an addon object make the card for it
 var genCard = (addon) => {
     var iconUrl = addon.icon_url;
@@ -24,19 +49,7 @@ var genCard = (addon) => {
         platformIcons += `<img src="./platformIcons/${platform}Icon.png" alt="${platform} icon" class="platformIcon">`
     })
 
-    var links = ``;
-    if(addon.modrinth_url != null) {
-        links += `<a target="_blank" href="${addon.modrinth_url}" class="addonLink modrinthLink">Modrinth</a><br>`
-    }
-    if(addon.curseforge_url != null) {
-        links += `<a target="_blank" href="${addon.curseforge_url}" class="addonLink curseforgeLink">CurseForge</a><br>`
-    }
-    if(addon.book_url != null){
-        links += `<a target="_blank" href="${addon.book_url}" class="addonLink bookLink">Book</a><br>`
-    }
-    if(addon.source_url != null){
-        links += `<a target="_blank" href="${addon.source_url}" class="addonLink sourceLink">Source</a><br>`
-    }
+    var links = makeLinks(addon);
 
     var card = `
     <div class="addonCard" id="${addon.name}Card">
@@ -46,7 +59,7 @@ var genCard = (addon) => {
         </div>
         <div class="platformShelf">${platformIcons}</div>
         <p class="addonDescription">${addon.description}</p>
-        <p class="linkShelf">${links}</p>
+        <div class="linkShelf">${links}</div>
     </div>
     `
     return card;
@@ -62,34 +75,22 @@ var genInteropCard = (addon) => {
     addon.platforms.forEach((platform) => {
         platformIcons += `<img src="./platformIcons/${platform}Icon.png" alt="${platform} icon" class="platformIcon">`
     })
-    if(addon.hex_provided){
-        platformIcons += "<span class='hexProvidedSpan'>|<img src='./otherIcons/hexxy.png' title='Hex Provided' alt='Hex Provided' class='platformIcon'></span>";
-    }
+    // if(addon.hex_provided){
+    //     platformIcons += "<span class='hexProvidedSpan'>|<img src='./otherIcons/hexxy.png' title='Hex Provided' alt='Hex Provided' class='platformIcon'></span>";
+    // }
 
-    var links = ``;
-    if(addon.modrinth_url != null) {
-        links += `<a target="_blank" href="${addon.modrinth_url}" class="addonLink modrinthLink">Modrinth</a><br>`
-    }
-    if(addon.curseforge_url != null) {
-        links += `<a target="_blank" href="${addon.curseforge_url}" class="addonLink curseforgeLink">CurseForge</a><br>`
-    }
-    if(addon.book_url != null){
-        links += `<a target="_blank" href="${addon.book_url}" class="addonLink bookLink">Book</a><br>`
-    }
-    if(addon.source_url != null){
-        links += `<a target="_blank" href="${addon.source_url}" class="addonLink sourceLink">Source</a><br>`
-    }
+    var links = makeLinks(addon);
     
 
     var card = `
     <div class="addonCard interopCard" id="${addon.name}Card">
         <div class="addonCardHeader">
-            <h3 class="addonTitle">${addon.name}
+            <h3 class="addonTitle${addon.hex_provided ? " hexProvidedTitle" : ""}">${addon.name}
             </h3>
         </div>
         <div class="platformShelf">${platformIcons}</div>
         <p class="addonDescription">${addon.description}</p>
-        <p class="linkShelf">${links}</p>
+        <div class="linkShelf">${links}</div>
     </div>
     `
     return card;
