f90f41eadbc83f1324dc61a29d2fe6da1c690f22
SamsTheNerd <sam@samsthenerd.com>
2023-12-28 02:50:31 -0500

    most of redesign done - made everything a bit smaller, added a toc, added some filters and sort modes, few other polishes

--- cardgen.js
+++ cardgen.js
@@ -1,20 +1,19 @@
 var ADDON_DATA = {};
 
 var ADDON_CARDS = {};
-var MAJOR_INTEROP_CARDS = {};
-var MINOR_INTEROP_CARDS = {};
+var INTEROP_CARDS = {};
 
 const ADDON_CARD_TYPES = {
     addon: ADDON_CARDS,
-    majorinterop: MAJOR_INTEROP_CARDS,
-    minorinterop: MINOR_INTEROP_CARDS
+    interop: INTEROP_CARDS
 }
 
 const ADDON_GRIDS = {
     addon: "mainAddonGrid",
-    majorinterop: "majorInteropGrid",
-    minorinterop: "minorInteropGrid"
+    interop: "interopGrid",
 }
+
+const PLATFORMS = ["fabric", "forge", "neoforge", "quilt"];
 
 // initializes stuff a good bit
 var getAddons = () => {
@@ -27,7 +26,28 @@ var getAddons = () => {
             ADDON_DATA[addon.name] = addon;
             getModData(addon).then((data) => {
                 console.log(`${addon.name} data: ${data}`);
-                ADDON_DATA[addon.name] = Object.assign(ADDON_DATA[addon.name], data);
+                Object.keys(data).forEach((key) => {
+                    // do some nonsense to get as many platforms as possible
+                    if( key == "platforms"){
+                        var platforms = [];
+                        if(data[key] != null){
+                            data[key].forEach((platform) => {
+                                if(!platforms.includes(platform))
+                                    platforms.push(platform);
+                            });
+                        }
+                        if(ADDON_DATA[addon.name][key] != null){
+                            ADDON_DATA[addon.name][key].forEach((platform) => {
+                                if(!platforms.includes(platform))
+                                    platforms.push(platform);
+                            });
+                        }
+                        ADDON_DATA[addon.name][key] = platforms;
+                    } else if(data[key] != null && ADDON_DATA[addon.name][key] == null){
+                        if(key == "book_url" && addon.type != "addon") ADDON_DATA[addon.name]["book_icon"] = "./otherIcons/MCBookIcon.png" // so interop wikis aren't hex books
+                        ADDON_DATA[addon.name][key] = data[key];
+                    }
+                });
                 // maybe some 're-sort' type of function needs to be called here
                 genCard(addon);
                 displayAddonCards(addon.type);
@@ -53,6 +73,16 @@ var getDatapacks = () => {
     request.send(null)
     var allDatapacks = JSON.parse(request.responseText);
     return allDatapacks;
+}
+
+// sort interop addons first by minor or not (so push ones that mostly just add recipes to the bottom) then by download count
+const interopSort = (a, b) => {
+    if(a.minor == b.minor){
+        var aDownloads = a.downloads == null ? 0 : a.downloads;
+        var bDownloads = b.downloads == null ? 0 : b.downloads;
+        return bDownloads - aDownloads;
+    }
+    return a.minor == true ? 1 : -1;
 }
 
 const SORT_FUNCTIONS = {
@@ -85,7 +115,11 @@ var displayAddonCards = (type = "addon", sortType = "downloads") => {
             addons.push(addon);
         }
     });
-    addons.sort(SORT_FUNCTIONS[sortType]);
+    if(type == "interop"){
+        addons.sort(interopSort);
+    } else {
+        addons.sort(SORT_FUNCTIONS[sortType]);
+    }
     addons.forEach((addon) => {
         var card = ADDON_CARD_TYPES[type][addon.name];
         document.getElementById(containerId).innerHTML += card;
@@ -123,20 +157,36 @@ var makeLinks = (addon) => {
     return links;
 }
 
+const DAY = 1000 * 60 * 60 * 24;
+
 // given an addon object make the card for it
 var genCard = (addon) => {
     var iconUrl = addon.icon_url;
     var platformIcons = ``;
+    var platformClasses = ``;
     if(addon.platforms != null){
+        addon.platforms.sort();
         addon.platforms.forEach((platform) => {
             platformIcons += `<img src="./platformIcons/${platform}Icon.png" alt="${platform} icon" class="platformIcon">`
+            platformClasses += ` ${platform}`;
         })
+    }
+    if(addon.type == "addon"){
+        platformClasses += " filterable"
     }
 
     var links = makeLinks(addon);
 
+    var banner = "";
+
+    if(Date.now() - addon.published_date < DAY * 30){
+        banner = `<img src="./otherIcons/new_banner.svg" class="newBanner cardBanner">`;
+    } else if(Date.now() - addon.updated_date < DAY * 7){
+        banner = `<img src="./otherIcons/update_banner.svg" class="updateBanner cardBanner">`;
+    }
+
     var card = `
-    <div class="addonCard" id="${addon.name}Card">
+    <div class="addonCard ${platformClasses}" id="${addon.name}Card">
         <div class="addonCardHeader">
             <img src="${iconUrl}" alt="${addon.name} icon" class="addonIcon">
             <h3 class="addonTitle${addon.hex_provided ? " hexProvidedTitle" : ""}">${addon.name}</h3>
@@ -144,6 +194,7 @@ var genCard = (addon) => {
         <div class="platformShelf">${platformIcons}</div>
         <p class="addonDescription">${addon.description}</p>
         <div class="linkShelf">${links}</div>
+        ${banner}
     </div>
     `
     ADDON_CARD_TYPES[addon.type][addon.name] = card;
--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 
+var putPlatformButtons = () => {
+    var platformButtons = "";
+    PLATFORMS.forEach((platform) => {
+        document.getElementById("body").classList.add(platform);
+        platformButtons += `<img src="./platformIcons/${platform}Icon.png" class="platformButton" id="${platform}Button" onclick="togglePlatform('${platform}')">`
+    })
+    document.getElementById("addonPlatformButtons").innerHTML = platformButtons;
+}
+
+var togglePlatform = (platform) => {
+    var othersSelected = false;
+    PLATFORMS.forEach((otherPlatform) => {
+        if(otherPlatform != platform && document.getElementById("body").classList.contains(otherPlatform)){
+            othersSelected = true;
+        }
+        document.getElementById("body").classList.remove(otherPlatform);
+    });
+    if(othersSelected){
+        document.getElementById("body").classList.add(platform);
+    } else {
+        PLATFORMS.forEach((otherPlatform) => {
+            document.getElementById("body").classList.add(otherPlatform);
+        });
+    }
+}
 
 window.onload = () => {
     var addons = getAddons();
@@ -12,6 +37,8 @@ window.onload = () => {
             putToolCard(tool, "toolsGrid");
         }
     });
+
+    putPlatformButtons();
 
     var datapacks = getDatapacks();
     datapacks.forEach((datapack) => {
